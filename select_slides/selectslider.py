99f7ea6fdb7b18a8cc4f81792a45a3c7f0f0a720
VIshwas Narayana <vishwasnarayan2345@gmail.com>
2021-04-29 23:59:05 +0530

    Create selectslider.py

---
+++ select_slides/selectslider.py
@@ -0,0 +1,8 @@
+improt streamlit as st
+st.title("StreamLit Select Slider")
+st.header("Using the python list to get the values")
+
+options_level = ["level-1","level-2","level-3"]
+
+initilise_slider_variable = st.select_slider("choose a level",options = options_level)
+st.write("you just chose : ",initilise_slider_variable)
