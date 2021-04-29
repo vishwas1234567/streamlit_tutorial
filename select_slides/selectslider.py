d95defaeac44ea6bd2352d34feaf3bc86ef0977e
VIshwas Narayana <vishwasnarayan2345@gmail.com>
2021-04-30 01:39:40 +0530

    Update selectslider.py

--- select_slides/selectslider.py
+++ select_slides/selectslider.py
@@ -86,5 +86,10 @@ if check_2:
   st.write(50*":fire:")
 
 
+  
+st.title("Drop Down")
 
 
+list_box = ["","option-1","option-2","option-3","option-4"]
+select_result = st.selectbox("select your option":list_box)
+st.write("you selected : ",select_result)
