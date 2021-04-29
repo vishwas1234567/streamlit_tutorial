3a4622b61aa357062524e43680ab709985e7c4a5
VIshwas Narayana <vishwasnarayan2345@gmail.com>
2021-04-30 00:17:41 +0530

    Update selectslider.py

--- select_slides/selectslider.py
+++ select_slides/selectslider.py
@@ -8,18 +8,7 @@ initilise_slider_variable = st.select_slider("choose a level",options = options_
 st.write("you just chose : ",initilise_slider_variable)
 
 
-#the two being deployed
-
-import streamlit as st
-st.title("StreamLit Select Slider")
-st.header("Using the python list to get the values")
-
-options_level = ["level-1","level-2","level-3"]
-
-initilise_slider_variable = st.select_slider("choose a level",options = options_level)
-st.write("you just chose : ",initilise_slider_variable)
-
-
+st.title("slider -2")
 
 #here we will start workign with the range to selct the data
 
